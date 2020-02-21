04a637bae1b21a722505eadeed45e6bcb7cf1fdf
Sean Wei <me@sean.taipei>
2020-02-21 15:20:27 +0800

    Add FAQ

---
+++ faq.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="zh-TW">
+	<head>
+<?php $TITLE = '常見問答'; include('includes/head.php'); ?>
+	</head>
+	<body>
+<?php include('includes/nav.php'); ?>
+		<header class="ts fluid vertically padded heading slate">
+			<div class="ts narrow container">
+				<h1 class="ts header">常見問答</h1>
+				<div class="description">靠北交大 2.0</div>
+			</div>
+		</header>
+		<div class="ts container" name="main">
+			<p>下面列出了幾個關於此服務的問題，如有疏漏可聯絡開發團隊，將儘快答覆您。</p>
+
+			<h2 class="ts header" id="modify-name">Q：如何更改暱稱</h2>
+			<p>目前此功能僅實作於 Telegram bot 中，請至首頁下方連結 Telegram 帳號。</p>
+			<p>於 Telegram 使用 /name 指令即可更改您的暱稱，所有過往的投稿、投票也會一起修正。</p>
+
+			<h2 class="ts header" id="facebook-sucks">Q：打不開 Facebook 貼文</h2>
+			<p>由於 Facebook 政策，發佈 App 之前必須先 <a target="_blank" href="/posts?id=4">審查身分證件</a>，已於 2/19 送件，等到審查通過後就能看得到了。</p>
+
+		</div>
+<?php include('includes/footer.php'); ?>
+	</body>
+</html>
--- includes/footer.php
+++ includes/footer.php
@@ -2,6 +2,7 @@
 	<center>
 		<p>由交大資工 112 級 <a target="_blank" href="https://www.sean.taipei/">Sean 韋詠祥</a> 開發設計<br />
 		聯絡信箱：<a href="mailto:x@nctu.app">x@nctu.app</a>
+		| <a href="/faq">常見問答</a>
 		| <a href="/policies">隱私權政策</a>
 		| 原始碼：<a href="https://git.io/xNCTU">Sea-n/xNCTU</a></p>
 	</center>
