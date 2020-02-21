f0936f8e97316efbf3d1d771b6aa72b1901001f2
Sean Wei <me@sean.taipei>
2020-02-21 15:56:26 +0800

    Check login state before ask reason

--- assets/js/review.js
+++ assets/js/review.js
@@ -2,34 +2,33 @@ function approve(uid) {
 	if (!confirm('您確定要通過此貼文嗎？'))
 		return;
 
-	var reason = prompt('請輸入通過附註');
-	if (reason.length == 0) {
-		alert('請勿留空');
-		return;
-	}
-
-	vote(uid, 1, reason);
+	vote(uid, 1, '請輸入通過附註');
 }
 
 function reject(uid) {
 	if (!confirm('您確定要駁回此貼文嗎？'))
 		return;
 
-	var reason = prompt('請輸入駁回理由');
+	vote(uid, -1, '請輸入駁回理由');
+}
+
+function vote(uid, type, reason_prompt) {
+	var login = document.querySelector('nav .right a[data-type="login"]');
+	if (login) {
+		alert('請先登入');
+		login.click();
+		return;
+	}
+
+	var reason = prompt(reason_prompt);
+	if (reason === null)
+		return;
+
 	if (reason.length < 5) {
 		alert('請輸入 5 個字以上');
 		return;
 	}
 
-	vote(uid, -1, reason);
-}
-
-function vote(uid, type, reason) {
-	var login = document.querySelector('nav .right a[data-type="login"]');
-	if (login) {
-		alert('請先登入');
-		login.click();
-	}
 
 	data = {
 		action: 'vote',
