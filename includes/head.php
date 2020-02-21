abe85adcc74a7f8eac0db411a75ebf65af9dfe3d
Sean Wei <me@sean.taipei>
2020-02-21 14:51:45 +0800

    Fix OG URL

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,18 @@ if (!isset($IMG))
 
 if (!isset($DESC))
 	$DESC = '不要問為何沒有人審文，先承認你就是沒有人。新版靠北交大 2.0 (xNCTU) 讓全校師生都有了審核的權限，每天穩定發出投稿文章。並支援 Telegram、Plurk、Twitter、Facebook 四大社群媒體平台。';
+
+$URL = $_SERVER['REQUEST_URI'];
+$URL = explode('?', $URL, 2);
+parse_str($URL[1] ?? '', $q);
+$URL = $URL[0];
+$query = [];
+foreach (['id', 'uid'] as $i)
+	if (isset($q[$i]))
+		$query[$i] = $q[$i];
+if (count($query) > 0)
+	$URL .= '?' . http_build_query($query);
+
 ?>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title><?= $TITLE ?> - 靠北交大 2.0 (xNCTU)</title>
@@ -15,13 +27,13 @@ if (!isset($DESC))
 <link rel="icon" type="image/png" href="/assets/img/logo-16.png" sizes="16x16">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 <meta name="keywords" content="xNCTU, 靠北交大 2.0" />
-<meta name="description" content="<?= $DESC ?>" />
-<meta property="og:title" content="<?= $TITLE ?>" />
-<meta property="og:url" content="https://x.nctu.app/" />
-<meta property="og:image" content="<?= $IMG ?>" />
-<meta property="og:image:secure_url" content="<?= $IMG ?>" />
+<meta name="description" content="<?= htmlentities($DESC) ?>" />
+<meta property="og:title" content="<?= htmlentities($TITLE) ?>" />
+<meta property="og:url" content="https://x.nctu.app<?= htmlentities($URL) ?>" />
+<meta property="og:image" content="<?= htmlentities($IMG) ?>" />
+<meta property="og:image:secure_url" content="<?= htmlentities($IMG) ?>" />
 <meta property="og:type" content="website" />
-<meta property="og:description" content="<?= $DESC ?>" />
+<meta property="og:description" content="<?= htmlentities($DESC) ?>" />
 <meta property="og:site_name" content="靠北交大 2.0" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tocas-ui/2.3.3/tocas.css">
 <link rel="stylesheet" href="/assets/css/style.css">
