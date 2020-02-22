46d7f43fb60ecba95dd1fc58de09084d69351e1c
Sean Wei <me@sean.taipei>
2020-02-22 09:41:10 +0800

    Add Sitemap

---
+++ sitemap.xml.php
@@ -0,0 +1,37 @@
+<?php
+require_once('database.php');
+$db = new MyDB();
+
+header('Content-Type: text/xml');
+
+$posts = $db->getPosts(50);
+?>
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<url>
+  <loc>https://x.nctu.app/</loc>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://x.nctu.app/posts</loc>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://x.nctu.app/submit</loc>
+  <priority>1.00</priority>
+</url>
+<?php foreach ($posts as $post) { ?>
+<url>
+  <loc>https://x.nctu.app/posts?id=<?= $post['id'] ?></loc>
+  <priority>0.87</priority>
+</url>
+<?php } ?>
+<url>
+  <loc>https://x.nctu.app/faq</loc>
+  <priority>0.69</priority>
+</url>
+<url>
+  <loc>https://x.nctu.app/policies</loc>
+  <priority>0.42</priority>
+</url>
+</urlset>
