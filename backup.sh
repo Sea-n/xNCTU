90f5bd8099085bb8f7bc492dedf1d5567d8e49ad
Sean Wei <me@sean.taipei>
2020-02-27 01:43:44 +0800

    Add images to auto backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 cd "`dirname $0`"
 
-cp sqlite.db backup/`date +%Y%m%d-%H%M`.json
+# Several times a day
+cp sqlite.db backup/sqlite-`date +%Y%m%d-%H%M`.db
+
+# Daily
+tar czf backup/img-`date +%Y%m%d`.tgz img
