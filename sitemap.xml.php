022f91270d1ac8e9d2459c1975c033b5e98cafc0
Sean Wei <me@sean.taipei>
2020-02-27 02:02:55 +0800

    Add ranking to sitemap

--- sitemap.xml.php
+++ sitemap.xml.php
@@ -4,7 +4,7 @@ $db = new MyDB();
 
 header('Content-Type: text/xml');
 
-$posts = $db->getPosts(50);
+$posts = $db->getPosts(0);
 ?>
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
@@ -28,6 +28,10 @@ $posts = $db->getPosts(50);
 <?php } ?>
 <url>
   <loc>https://x.nctu.app/faq</loc>
+  <priority>0.69</priority>
+</url>
+<url>
+  <loc>https://x.nctu.app/ranking</loc>
   <priority>0.69</priority>
 </url>
 <url>
