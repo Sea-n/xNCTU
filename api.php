8195dbbd795e2eb669821b7ca85552f30ed8f031
Sean Wei <me@sean.taipei>
2020-03-07 18:31:16 +0800

    Redirect only if posted to all platforms

--- api.php
+++ api.php
@@ -64,7 +64,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 		$post = $db->getPostByUid($uid);
 		$result['approvals'] = (int) $post['approvals'];
 		$result['rejects'] = (int) $post['rejects'];
-		if (isset($post['id']))
+
+		/* Redirect if posted to all platforms */
+		if ($post['status'] == 5)
 			$result['id'] = $post['id'];
 
 		$result['votes'] = [];
