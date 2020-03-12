40ce3891ace679be164a3e0dbcaf401cf7bfd227
Sean Wei <me@sean.taipei>
2020-03-12 21:20:31 +0800

    Add static version of /posts page

---
+++ static-posts.php
@@ -0,0 +1,48 @@
+<?php
+require_once('utils.php');
+require_once('database.php');
+$db = new MyDB();
+
+$posts = $db->getPosts(0);
+
+?>
+<!DOCTYPE html>
+<html lang="zh-TW">
+	<head>
+<?php
+$TITLE = '文章列表';
+$IMG = 'https://x.nctu.app/assets/img/og.png';
+include('includes/head.php');
+?>
+	</head>
+	<body>
+<?php include('includes/nav.php'); ?>
+		<header class="ts fluid vertically padded heading slate">
+			<div class="ts narrow container">
+				<h1 class="ts header">文章列表</h1>
+				<div class="description">靠北交大 2.0</div>
+			</div>
+		</header>
+
+		<div class="ts container" name="main">
+			<div id="posts">
+<?php
+foreach ($posts as $post) {
+?>
+				<div class="ts card" id="post-<?= $post['id'] ?>" style="margin-bottom: 42px;">
+<?php if ($post['has_img']) { ?>
+					<div class="image">
+						<img id="img" class="post-image" style="max-height: 60vh; width: auto;" src="/img/<?= $post['uid'] ?>.jpg" />
+					</div>
+<?php } ?>
+					<div class="content">
+						<div class="header"><a id="hashtag" href="/post/<?= $post['id'] ?>">#靠交<?= $post['id'] ?></a></div>
+						<div id="body"><?= toHTML($post['body']) ?></div>
+					</div>
+				</div>
+<?php } ?>
+			</div>
+		</div>
+<?php include('includes/footer.php'); ?>
+	</body>
+</html>
