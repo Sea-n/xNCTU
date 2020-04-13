e888c293a704941bc8920c712fd9614fb171e4ac
Sean Wei <me@sean.taipei>
2020-04-13 21:43:41 +0800

    Update backup script

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 cd "`dirname $0`"
 
 # Several times a day
-mysqldump xnctu |gzip > backup/database-`date +%Y%m%d-%H%M`.sql.gz
+mysqldump xnctu |gzip > backup/database/database-`date +%Y%m%d-%H%M`.sql.gz
 
-# Daily
-tar czf backup/img-`date +%Y%m%d`.tgz img
+# Monthly
+tar czf backup/img/img-`date +%Y%m`.tgz img
