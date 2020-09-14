443167a080a7c6107116ab89aef2232c985eba85
Sean Wei <me@sean.taipei>
2020-09-14 11:10:05 +0800

    Update transparency report

--- transparency.php
+++ transparency.php
@@ -87,6 +87,7 @@ $IMG = "https://$DOMAIN/assets/img/og.png";
 					</tr>
 				</thead>
 				<tbody>
+					<tr><td>2020 Sep</td><td>0</td><td>0</td><td>0</td><td>0</td></tr>
 					<tr class="negative indicated"><td>2020 Aug</td><td>0</td><td>1</td><td>0</td><td>0</td></tr>
 					<tr><td>2020 Jul</td><td>0</td><td>0</td><td>0</td><td>0</td></tr>
 					<tr class="negative indicated"><td>2020 Jun</td><td>0</td><td>1</td><td>1</td><td>0</td></tr>
@@ -99,7 +100,7 @@ $IMG = "https://$DOMAIN/assets/img/og.png";
 
 			<br>
 			<p>收到任何刪除通知將人工更新至此頁面，在不造成二次傷害的前提下，本站會盡可能提供最多資訊，原則上收到請求後會在 7 天內公開揭露。</p>
-			<p style="text-align: right;"><i>最後更新日期：2020 Aug 21</i></p>
+			<p style="text-align: right;"><i>最後更新日期：2020 Sep 14</i></p>
 		</div>
 <?php include('includes/footer.php'); ?>
 	</body>
