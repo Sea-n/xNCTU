6c64b7c718b5db622d6780bc6482bd807475670f
Sean Wei <me@sean.taipei>
2020-10-26 15:30:48 +0800

    Add self censorship to transparency report
    Fuck you Facebook.

--- transparency.php
+++ transparency.php
@@ -67,6 +67,61 @@ include('includes/header.php');
 				</tbody>
 			</table>
 
+			<h2>因應 Facebook 政策自我審查</h2>
+			<p>此清單中的貼文僅會從 Facebook、Instagram 下架，您仍然可以從 Telegram、Twitter、Plurk 看到原文。</p>
+			<table class="ts striped table">
+				<thead>
+					<tr>
+						<th>日期</th>
+						<th>內容節錄</th>
+						<th>貼文編號</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>2020 Oct 26</td>
+						<td>草屯警兒田勝傑強姦女學生</td>
+						<td>
+							<a href="https://x.nctu.app/post/4993" target="_blank">#靠交4993</a>、
+							<a href="https://x.nctu.app/post/5356" target="_blank">#靠交5356</a>、
+							<a href="https://x.nctu.app/post/5369" target="_blank">#靠交5369</a>、
+							<a href="https://x.nctu.app/post/5371" target="_blank">#靠交5371</a>、
+							<a href="https://x.nctu.app/post/5376" target="_blank">#靠交5376</a>、
+							<a href="https://x.nctu.app/post/5377" target="_blank">#靠交5377</a>、
+							<a href="https://x.nctu.app/post/5379" target="_blank">#靠交5379</a>、
+							<a href="https://x.nctu.app/post/5380" target="_blank">#靠交5380</a>、
+							<a href="https://x.nctu.app/post/5385" target="_blank">#靠交5385</a>、
+							<a href="https://x.nctu.app/post/5393" target="_blank">#靠交5393</a>、
+							<a href="https://x.nctu.app/post/5404" target="_blank">#靠交5404</a>、
+							<a href="https://x.nctu.app/post/5407" target="_blank">#靠交5407</a>、
+							<a href="https://x.nctu.app/post/5410" target="_blank">#靠交5410</a>、
+							<a href="https://x.nctu.app/post/5412" target="_blank">#靠交5412</a>、
+							<a href="https://x.nctu.app/post/5413" target="_blank">#靠交5413</a>、
+							<a href="https://x.nctu.app/post/5416" target="_blank">#靠交5416</a>、
+							<a href="https://x.nctu.app/post/5429" target="_blank">#靠交5429</a>、
+							<a href="https://x.nctu.app/post/5444" target="_blank">#靠交5444</a>、
+							<a href="https://x.nctu.app/post/5453" target="_blank">#靠交5453</a>、
+							<a href="https://x.nctu.app/post/5455" target="_blank">#靠交5455</a>、
+							<a href="https://x.nctu.app/post/5457" target="_blank">#靠交5457</a>、
+							<a href="https://x.nctu.app/post/5463" target="_blank">#靠交5463</a>、
+							<a href="https://x.nctu.app/post/5479" target="_blank">#靠交5479</a>、
+							<a href="https://x.nctu.app/post/5485" target="_blank">#靠交5485</a>、
+							<a href="https://x.nctu.app/post/5487" target="_blank">#靠交5487</a>
+						</td>
+					</tr>
+					<tr>
+						<td>2020 Jun 11</td>
+						<td>請支援收淫</td>
+						<td><a href="https://x.nctu.app/post/2275" target="_blank">#靠交2275</a></td>
+					</tr>
+					<tr>
+						<td>2020 Jun 06</td>
+						<td>高雄罷韓催票（當日）</td>
+						<td><a href="https://x.nctu.app/post/2144" target="_blank">#靠交2144</a></td>
+					</tr>
+				</tbody>
+			</table>
+
 			<h2>維護團隊主動刪除</h2>
 			<table class="ts striped table">
 				<thead>
@@ -114,7 +169,7 @@ include('includes/header.php');
 
 			<br>
 			<p>收到任何刪除通知將人工更新至此頁面，在不造成二次傷害的前提下，本站會盡可能提供最多資訊，原則上收到請求後會在 7 天內公開揭露。</p>
-			<p style="text-align: right;"><i>最後更新日期：2020 Oct 15</i></p>
+			<p style="text-align: right;"><i>最後更新日期：2020 Oct 26</i></p>
 		</div>
 <?php include('includes/footer.php'); ?>
 	</body>
