11005939dda465399ada394375ec360dbdd69c96
CSY54 <alex35748954@gmail.com>
2021-09-02 22:59:15 +0800

    Modify input style for post-searching form

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -27,42 +27,45 @@ $posts = $query->orderByDesc('id')->take(50)->get();
 
 @section('content')
     <form action="" method="GET">
-        <div class="ts labeled icon button">
-            <i class="image icon"></i>
-            <span class="text">
-                多媒體類型
-                <select name="media">
-                    <option value="" selected>不限</option>
-                    <option value="0">純文字</option>
-                    <option value="1">圖片</option>
-                </select>
-            </span>
+        <div class="ts labeled left action input">
+            <div class="ts label">
+                <i class="image icon"></i>
+                <span class="text">多媒體類型</span>
+            </div>
+            <select name="media" class="ts basic dropdown">
+                <option value="" selected>不限</option>
+                <option value="0">純文字</option>
+                <option value="1">圖片</option>
+            </select>
         </div>
 
-        <div class="ts labeled icon button">
-            <i class="like outline icon"></i>
-            <span class="text">
-                貼文讚數
-                <select name="likes">
-                    <option value="" selected>不限</option>
-                    <option value="10">10</option>
-                    <option value="25">25</option>
-                    <option value="50">50</option>
-                    <option value="100">100</option>
-                    <option value="200">200</option>
-                    <option value="500">500</option>
-                </select>
-            </span>
+        <div class="ts labeled left action input">
+            <div class="ts label">
+                <i class="like outline icon"></i>
+                <span class="text">貼文讚數</span>
+            </div>
+            <select name="likes" class="ts basic dropdown">
+                <option value="" selected>不限</option>
+                <option value="10">10</option>
+                <option value="25">25</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+                <option value="200">200</option>
+                <option value="500">500</option>
+            </select>
         </div>
 
-        <div class="ts labeled icon button">
-            <i class="search icon"></i>
-            <span class="text">
-                關鍵字
-                <input name="keyword" placeholder="武漢肺炎" size=8>
-            </span>
+        <div class="ts labeled left action input">
+            <div class="ts label">
+                <i class="search icon"></i>
+                <span class="text">關鍵字</span>
+            </div>
+            <input name="keyword" placeholder="武漢肺炎" size="8">
         </div>
-        <button class="ts primary button" type="submit">送出</button>
+
+        <button class="ts primary button" type="submit" style="margin-top: -3px;">
+            送出
+        </button>
     </form>
     <br>
 
